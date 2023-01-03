4c5da025c49589be3076c0b2617c8bb2206c4cd2
LR <leo.russi@advantivesystems.com>
2023-01-03 18:04:04 +0400

    favicon, meta, robots

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>SebRussi Fitness - Let's workout!</title>
+        <meta name='description' content='Fitness services, plans and apparel'/>
+        <meta name='keywords' content='fitness, gym, body building, calistenics, gym apparel, apparel, sport, training'/>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
